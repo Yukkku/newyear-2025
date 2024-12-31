82b717b39a2cbf9535a0a23d7b82cb8f6aa29809
Yukkku <76182771+Yukkku@users.noreply.github.com>
2025-01-01 00:00:46 +0900

    Add files via upload

---
+++ 2025.js
@@ -0,0 +1,25 @@
+var[s,a]=[421,`var[b,c,d,e,f,g,h,i,j,k,o,z]=atob('YFwgCg');k=423;f=atob("L3p6Ri0
+PWmBRSw8QeBkPDw8PDxlaYEs7Ilr2G0JCKC0PTVM1LQ8RaRkPDw8PDxlNUy07IlkcYiYmGS0PQEUnHg8
+S                                                                              W
+h     kPDw8PDxlA         RR47IkodVQ         oKCi0PMzcZ     Dw8TSxkPDw8PDxkzNw  8
+7   IjseSw8ADwAPAM     MPJh4PDw8PDxQ8     D6UPDw8PD7weDw   8t8BIsH+EAD5YAww8Z  D
+w  8PDw8PFS0PpQ8PDw   8Prw8PDy3wEg9a8B   gPD4cPww8PDw8PDw  8PDxYeD6UPDw8PD6UP  D
+w 8PLfAS8BHwFx4PeCRz Dw8PDw8PDw8XDxkPDw 8PDxkPDw8PDzsiOxzS LQ9pNVUPDw8PDw8PDx  g
+A GQ8PDw8P  GQ8PDw8P OyI7HMM8  D1pFRg8P GRkZHRoZ  Dw8PGRkP DxkPOyI7            H
+L           RLD0tVNw 8PGCc1Hg  kZDw8PJi           gPDxgeOy I7HKVaDz            x
+a          Dy0PGRc1  US0XD6UP  DzMtrxge          LfAePPAS  aQ8t8BIt            D
+x         kVUXw8JQ   +lDw9APK  8ZDy3wHj         zwEXgPHv   ATLQ8ZD6XjSzMP      s
+x        xNS68PDw    8t8BIdIj  yHDw+HDy        0PGQ+l8B    FaQQ/CGmlarw8Pw/    A
+S       HSI3CgoK     LQ8PDw+l  8BFaTxk7       GHhkDw8P     Dw87Ih0iNxcZFzwPD   w
+8      PpfARWl0      ZShaHZA8  PDw8PSiA      sIjckKCR      LDw8PD6XwEVprGVeWZ  A
+8     PDw8PWR7       wE9w+Rj5  pDw8PD6X     wEVp5GVo       PpWQPDw8  PD3ca8BU  "
+)    ;g=b.rep        eat(k*2)  +d.repea    t(k);o=(                  )=>f.cha  r
+C   odeAt(i+         +);for(i  =0;f[i];   )h=o(),g                   +=d.repe  a
+t  (h>239?o          ():h/15+  1)+b.rep  eat(h%15          -14?h%15  +8:o());  g
++ =d.repeat(438);set Interval(()=>{cons ole.clear();z="var [s,a]=["+s+","+b+a  .
+s lice(0,1209)+a;f="  ";h=0;for(i=0;i<  25;i++,f+=e)for(j=  0;j<80;j++)f+=j%   7
+9 >0&g[i*k+(j+s)%k]=   =d?d:z[h++];co   nsole.log(f.slice(   0,1969)+b+".re    p
+l ace(/"+c+"s/g,'')]     ;eval(a)//     Copyright_(c)_2025     _Yukkku");      s
+=                                                                              +
++s%k},50)//var[b,c,d,e,f,g,h,i,j,k,o,z]=atob('YFwgCg');k=423;f=atob("L3p6Ri0PWmB
+RSw8QeBkPDw8PDxlaYEs7Ilr2`.replace(/\s/g,'')];eval(a)//Copyright_(c)_2025_Yukkku
---
+++ README.md
@@ -0,0 +1,14 @@
+# newyear-2025
+
+プログラマに捧げる年賀状 (2025年版)
+
+[2023年版](https://github.com/Yukkku/2023NewYear) -
+[2024年版](https://github.com/Yukkku/newyear-2024)
+
+## 楽しみ方
+
+```sh
+node 2025.js
+```
+
+で動きます. プログラムのサイズは2025byteです.
